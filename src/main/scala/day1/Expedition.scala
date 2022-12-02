7c985fc381c999ba17bdb1e1ffabc7515f1d12af
Kevin Riley <kp.riley@icloud.com>
2022-12-01 20:45:33 -0800

    Update Expedition.scala

--- src/main/scala/day1/Expedition.scala
+++ src/main/scala/day1/Expedition.scala
@@ -8,13 +8,10 @@ import scala.annotation.tailrec
 object Expedition {
   def apply(i: Int): Elf  = elves(i)
 
+  // Load Config File
   private val applicationConf: Config = ConfigFactory.load("application.conf")
 
-  /*** Originally setup a random Id generator,
-        but removing to asuage confusion from getting different numbers
-        from repeat runs
-  */
-
+  // Create lazy sequence for Id generation
   private def genSeqId(n: Int): LazyList[Int] =
     n #:: genSeqId(
       n + rng42.between(1, applicationConf.getInt("sequence.randomRange"))
@@ -25,6 +22,7 @@ object Expedition {
   private var idIndex: Int = 0
   private def nextId: Int = { idIndex += 1; seqIds(idIndex) }
 
+  // Parse Input Data and create List of case classes
   lazy val elves: List[Elf] =
     @tailrec
     def recur(xs: List[String], currElf: Elf, acc: List[Elf] = Nil): List[Elf] = xs match
@@ -32,11 +30,12 @@ object Expedition {
       case head :: tail if head.isBlank => recur(tail, Elf(nextId), currElf :: acc)
       case head :: tail => recur(tail, currElf.addSnack(head.toInt), acc)
 
+    // Initate recursive function from text stream
     util.Using(io.Source.fromResource("day1/input-data.txt")){
       stream => recur(stream.getLines.toList, Elf(0))
     }.getOrElse(Nil)
 
-
+  // Calculate metrics
   lazy val snackiest: List[Elf] = elves.sortBy(-_.kcals)
   def totalSnackiness(top: Int): Int = snackiest.take(top).map(_.kcals).sum
   def totalSnackiness: Int = snackiest.map(_.kcals).sum
