763c5a8af409d5c1ec51c0da89c28e7a74b89ab9
Kevin Riley <kp.riley@icloud.com>
2022-12-04 10:46:54 -0800

    Added day3 package

---
+++ src/main/scala/day3/RucksackReorganization.scala
@@ -0,0 +1,15 @@
+package works.redcode
+package day3
+
+@main def RucksackReorganization(): Unit =
+
+  println(s"""
+    ========================================================
+    Part 1 total score is:
+
+
+    Part 2:
+    TBD
+    ========================================================
+    """
+  )
