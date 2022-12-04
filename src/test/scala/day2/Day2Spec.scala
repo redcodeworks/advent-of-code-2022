51c2168bff8e9fb43da8ad90fc2aa7a8a8926463
Kevin Riley <kp.riley@icloud.com>
2022-12-03 17:36:26 -0800

    Created stub for Day2 test suite

---
+++ src/test/scala/day2/Day2Spec.scala
@@ -0,0 +1,10 @@
+package works.redcode
+package day2
+
+import org.scalatest.*
+import org.scalatest.flatspec.*
+import org.scalatest.matchers.*
+
+import scala.collection.mutable.Stack
+
+class Day2Spec extends AnyFlatSpec with should.Matchers {}
