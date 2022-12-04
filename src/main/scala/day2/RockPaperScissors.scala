1dd68cbbdcf4fbf36ea6e98f806c94087bdbe220
Kevin Riley <kp.riley@icloud.com>
2022-12-03 19:43:17 -0800

    Finished Part 1 of Day 2

---
+++ src/main/scala/day2/RockPaperScissors.scala
@@ -0,0 +1,12 @@
+package works.redcode
+package day2
+
+@main def RockPaperScissors(): Unit =
+
+  println(s"""
+    ========================================================
+    The total score is...
+
+    ========================================================
+    """
+  )
