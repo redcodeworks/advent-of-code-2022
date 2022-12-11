b2131246e2baf1c664d393982491513612c781ca
Kevin Riley <kp.riley@icloud.com>
2022-12-10 18:35:59 -0800

    Finished Day 6

---
+++ src/main/scala/day6/TuningHandler.scala
@@ -0,0 +1,17 @@
+package works.redcode
+package day6
+
+object TuningHandler:
+  val encrypted = io.Source.fromResource("day6/input-data.txt").toList
+
+
+  def buildSet(encrypted: List[Char], setSize: Int) =
+    def recur(xs: List[Char], currentSet: List[Char], i: Int = setSize): (Int, String) = currentSet match
+      case Nil => (i, "")
+      case head :: tail if currentSet.toSet.size == setSize => (i, currentSet.mkString)
+      case head :: tail => recur(xs.drop(1), tail :+ xs.head, i + 1)
+
+    recur(encrypted.drop(setSize), encrypted.take(setSize).toList)
+
+
+  def decrypted(setSize: Int) = buildSet(encrypted, setSize)
---
+++ src/main/scala/day6/TuningTrouble.scala
@@ -0,0 +1,22 @@
+package works.redcode
+package day6
+
+@main def TuningTrouble(): Unit =
+
+  println(s"""
+    DAY 5: Camp Supply Stacks
+    ========================================================
+    Part 1:
+    ${TuningHandler.decrypted(4)}
+
+    Part 2:
+    ${TuningHandler.decrypted(14)}
+
+    ========================================================
+
+    """
+  )
+
+
+
+
